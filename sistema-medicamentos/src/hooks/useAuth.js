d9d2f3eb76b7bd246c72d9c0d751b84e52b31775
Joao Batista <126587508+Joaobneto1@users.noreply.github.com>
2025-05-04 20:55:49 -0300

    add pageReload fix

--- sistema-medicamentos/src/hooks/useAuth.js
+++ sistema-medicamentos/src/hooks/useAuth.js
@@ -2,32 +2,42 @@ import { useState, useEffect } from "react";
 
 const useAuth = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(() => {
-        // Inicializa o estado com base no localStorage
         const storedUser = JSON.parse(localStorage.getItem("user"));
         return storedUser ? true : false;
     });
 
     const [user, setUser] = useState(() => {
-        // Restaura o usuário do localStorage
         return JSON.parse(localStorage.getItem("user")) || null;
     });
 
     useEffect(() => {
         // Atualiza o localStorage sempre que o estado de login ou usuário mudar
         if (isLoggedIn && user) {
-            const { email } = user; // Salva apenas o email ou outros dados simples
+            const { email } = user;
             localStorage.setItem("user", JSON.stringify({ email }));
         } else {
             localStorage.removeItem("user");
         }
     }, [isLoggedIn, user]);
 
+    // Limpa o estado de autenticação ao recarregar a página
+    useEffect(() => {
+        const handlePageReload = () => {
+            setIsLoggedIn(false);
+            setUser(null);
+            localStorage.removeItem("user");
+        };
+
+        // Executa a limpeza ao carregar a página
+        handlePageReload();
+    }, []);
+
     const handleLoginSubmit = (data) => {
-        const loginSuccessful = data && data.email; // Simula sucesso no login
+        const loginSuccessful = data && data.email;
         if (loginSuccessful) {
-            setUser(data); // Atualiza o estado do usuário
-            setIsLoggedIn(true); // Define o estado de login como verdadeiro
-            const { email } = data; // Salva apenas o email no localStorage
+            setUser(data);
+            setIsLoggedIn(true);
+            const { email } = data;
             localStorage.setItem("user", JSON.stringify({ email }));
         } else {
             console.error("Erro no login: Dados inválidos");
@@ -36,9 +46,9 @@ const useAuth = () => {
     };
 
     const handleLogout = () => {
-        setIsLoggedIn(false); // Define o estado de login como falso
-        setUser(null); // Limpa o estado do usuário
-        localStorage.removeItem("user"); // Remove o usuário do localStorage
+        setIsLoggedIn(false);
+        setUser(null);
+        localStorage.removeItem("user");
     };
 
     return {
