4d57a90a041c087647536dbdc6ccbbfc70eed424
leonardo lima <leonardo.limal15@hotmail.com>
2025-05-07 15:46:14 -0300

    organização de rota

--- sistema-medicamentos/src/App.js
+++ sistema-medicamentos/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 import HorarioAtual from "./components/Horario/horarioAtual";
@@ -7,8 +8,10 @@ import Login from "./components/Auth/login";
 import PacienteList from "./components/Pacientes/pacientList";
 import SignUp from "./components/Auth/signUp";
 import useAppLogic from "./hooks/appLogic";
-import useAuth from "./hooks/useAuth"; // Importa o hook de autenticação
-import MedicamentoList from "./components/Medicamentos/medicamentoList"; // Importação do componente de lista de medicamentos
+import useAuth from "./hooks/useAuth";
+import MedicamentoCadastro from "./components/Medicamentos/medicamentoCadastro";
+import EstoqueMedicamentos from "./components/Medicamentos/estoqueMedicamentos";
+import routes from "./routesConfig"; // Importar as rotas configuradas
 
 function App() {
   const { isLoggedIn, user, handleLoginSubmit, handleLogout } = useAuth();
@@ -33,86 +36,106 @@ function App() {
     setShowForm,
   } = useAppLogic();
 
-  const renderContent = () => {
-    if (!isLoggedIn) {
-      return isSignUp ? (
-        <SignUp
-          signUpData={signUpData}
-          handleSignUpChange={handleSignUpChange}
-          handleSignUpSubmit={handleSignUpSubmit}
-          signUpError={signUpError}
-          setIsSignUp={setIsSignUp}
-        />
-      ) : (
-        <Login
-          loginData={loginData}
-          handleLoginChange={handleLoginChange}
-          handleLoginSubmit={(e) => {
-            e.preventDefault();
-            const loginSuccessful = handleLoginSubmit(loginData);
-            if (!loginSuccessful) {
-              console.error("Falha no login");
-            }
-          }}
-          loginError={loginError}
-          setIsSignUp={setIsSignUp}
-        />
-      );
-    }
-
-    if (activeTab === "Paciente") {
-      return (
-        <PacienteList
-          pacientes={pacientes}
-          showForm={showForm}
-          formData={formData}
-          handleInputChange={handleInputChange}
-          handleFormSubmit={handleFormSubmit}
-          setShowForm={setShowForm}
-        />
-      );
-    }
-
-    if (activeTab === "Medicamentos") {
-      return <MedicamentoList />; // Renderiza o componente de lista de medicamentos
-    }
-
-    return <h1>Bem-vindo ao Sistema de Medicamentos</h1>;
-  };
-
   return (
-    <div className="app">
-      <header className="header">
-        <div className="header-left"></div> {/* Título do sistema */}
-        <div className="header-center">
-          <HorarioAtual /> {/* Exibe o horário atualizado no centro */}
-        </div>
-        <div className="header-right">
-          {isLoggedIn && user ? (
-            <div style={{ display: "flex", alignItems: "center" }}>
-              <span>{`Olá, ${user.email}`}</span> {/* Exibe o usuário logado */}
-              <button onClick={handleLogout} className="logout-button">
-                <FontAwesomeIcon icon={faSignOutAlt} />
-              </button>
-            </div>
-          ) : (
-            "Não logado"
-          )}
-        </div>
-      </header>
-      <nav className="navbar">
-        <button className="nav-button" onClick={() => setActiveTab("Paciente")}>
-          Paciente
-        </button>
-        <button
-          className="nav-button"
-          onClick={() => setActiveTab("Medicamentos")}
-        >
-          Medicamentos
-        </button>
-      </nav>
-      <main className="content">{renderContent()}</main>
-    </div>
+    <Router>
+      <div className="app">
+        <header className="header">
+s          <div className="header-left">
+            <Link to={routes.login} className="nav-link">
+              Login
+            </Link>
+          </div>
+          <div className="header-center">
+            <HorarioAtual />
+          </div>
+          <div className="header-right">
+            {isLoggedIn && user ? (
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <span>{`Olá, ${user.email}`}</span>
+                <button onClick={handleLogout} className="logout-button">
+                  <FontAwesomeIcon icon={faSignOutAlt} />
+                </button>
+              </div>
+            ) : (
+              "Não logado"
+            )}
+          </div>
+        </header>
+        <nav className="navbar">
+          <Link to={routes.home} className="nav-link">
+            Home
+          </Link>
+          <Link to={routes.pacientes} className="nav-link">
+            Pacientes
+          </Link>
+          <Link to={routes.cadastroMedicamentos} className="nav-link">
+            Cadastro de Medicamentos
+          </Link>
+          <Link to={routes.estoqueMedicamentos} className="nav-link">
+            Estoque de Medicamentos
+          </Link>
+        </nav>
+        <main className="content">
+          <Routes>
+            <Route
+              path={routes.home}
+              element={<h1>Bem-vindo ao Sistema de Medicamentos</h1>}
+            />
+            <Route
+              path={routes.login}
+              element={
+                <Login
+                  loginData={loginData}
+                  handleLoginChange={handleLoginChange}
+                  handleLoginSubmit={(e) => {
+                    e.preventDefault();
+                    const loginSuccessful = handleLoginSubmit(loginData);
+                    if (!loginSuccessful) {
+                      console.error("Falha no login");
+                    }
+                  }}
+                  loginError={loginError}
+                  setIsSignUp={setIsSignUp}
+                />
+              }
+            />
+            <Route
+              path={routes.signUp}
+              element={
+                <SignUp
+                  signUpData={signUpData}
+                  handleSignUpChange={handleSignUpChange}
+                  handleSignUpSubmit={handleSignUpSubmit}
+                  signUpError={signUpError}
+                  setIsSignUp={setIsSignUp}
+                />
+              }
+            />
+            <Route
+              path={routes.pacientes}
+              element={
+                <PacienteList
+                  pacientes={pacientes}
+                  showForm={showForm}
+                  formData={formData}
+                  handleInputChange={handleInputChange}
+                  handleFormSubmit={handleFormSubmit}
+                  setShowForm={setShowForm}
+                />
+              }
+            />
+            <Route
+              path={routes.cadastroMedicamentos}
+              element={<MedicamentoCadastro />}
+            />
+            <Route
+              path={routes.estoqueMedicamentos}
+              element={<EstoqueMedicamentos />}
+            />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
